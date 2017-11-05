b0577dd45585a49f03574138b8165f88d8709955
Anthony Bond <ajbond2005@gmail.com>
2017-11-05 16:15:37 -0500

    Include pagination on requests

--- contrib/inventory/digital_ocean.py
+++ contrib/inventory/digital_ocean.py
@@ -165,18 +165,32 @@ class DoManager:
     def _url_builder(self, path):
         if path[0] == '/':
             path = path[1:]
-        return '%s/%s' % (self.api_endpoint, path)
+        return '%s/%s?per_page=200' % (self.api_endpoint, path)
 
     def send(self, url, method='GET', data=None):
         url = self._url_builder(url)
         data = json.dumps(data)
         try:
             if method == 'GET':
-                resp = requests.get(url, data=data, headers=self.headers, timeout=self.timeout)
-                json_resp = resp.json()
+                resp_data = {}
+                while True:
+                    resp = requests.get(url, data=data, headers=self.headers, timeout=self.timeout)
+                    json_resp = resp.json()
+
+                    for key, value in json_resp.items():
+                        if isinstance(value, list) and key in resp_data:
+                            resp_data[key] += value
+                        else:
+                            resp_data[key] = value
+
+                    try:
+                        url = json_resp['links']['pages']['next']
+                    except KeyError:
+                        break
+                return resp_data
+
         except ValueError as e:
             sys.exit("Unable to parse result from %s: %s" % (url, e))
-        return json_resp
 
     def all_active_droplets(self):
         resp = self.send('droplets/')
